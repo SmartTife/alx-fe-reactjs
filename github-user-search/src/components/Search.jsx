53b7b5e4e00ec25c5558701056ee7e8e8d6b9d87
SmartTife <ayoadeiradatboluwatife@gmail.com>
2025-08-01 02:45:50 +0100

    Removed the apostrophe in the cant in search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
       const userData = await fetchUserData(username.trim());
       setUser(userData);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
