8af9c5a34e9dd409498b7a4805c2564b8ced4788
SmartTife <ayoadeiradatboluwatife@gmail.com>
2025-08-24 23:48:47 +0100

    Fixed a bug

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,6 @@
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const isAuthenticated = true; // change to false to simulate redirect
-
-export default function ProtectedRoute({ children }) {
-  if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
-  }
-  return children;
+export default function ProtectedRoute({ isAuthenticated, children }) {
+  return isAuthenticated ? children : <Navigate to="/login" />;
 }
