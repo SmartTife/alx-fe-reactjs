a73a408f1358a86c330ffa61dbd18aec8116b615
SmartTife <ayoadeiradatboluwatife@gmail.com>
2025-08-24 10:40:55 +0100

    Another debugging done

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4 text-center">User Registration</h1>
-      {/* Uncomment the form you want to use */}
-      {/* <RegistrationForm /> */}
+      
+      
       <FormikForm />
     </div>
   );
